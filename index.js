bb3413584f3795728c7920052b5b01a3b40a7597
Chris Troutner <chris.troutner@gmail.com>
2020-06-07 19:36:59 -0700

    fix(encryption): Added encryption to wallet

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 'use strict'
 
 const BCHJS = require('@chris.troutner/bch-js')
-// const CryptoJS = require('crypto-js')
+const crypto = require('crypto-js')
 
 let _this
 
@@ -16,36 +16,61 @@ class MinimalBCHWallet {
 
     this.HdPath = this.advancedOptions.HdPath || "m/44'/245'/0'/0/0"
 
-    // Encapsulae the bchjs library.
+    // Encapsulae the external libraries.
     this.BCHJS = BCHJS
     this.bchjs = new BCHJS()
+    this.crypto = crypto
 
     _this = this
 
-    this.create(hdPrivateKeyOrMnemonic)
+    this.walletInfo = this.create(hdPrivateKeyOrMnemonic)
   }
 
   // Create a new wallet.
   async create (mnemonic) {
-    // if (mnemonic && this.advancedOptions.password) {
-    //   mnemonic = SimpleWallet.decrypt(mnemonic, this.advancedOptions.password);
-    // }
+    // Attempt to decrypt mnemonic if password is provided.
+    if (mnemonic && this.advancedOptions.password) {
+      mnemonic = this.decrypt(mnemonic, this.advancedOptions.password)
+    }
 
+    // Generate the HD wallet.
     mnemonic = mnemonic || _this.bchjs.Mnemonic.generate(128)
     const rootSeedBuffer = await _this.bchjs.Mnemonic.toSeed(mnemonic)
     const masterHDNode = _this.bchjs.HDNode.fromSeed(rootSeedBuffer)
     const childNode = masterHDNode.derivePath(this.HdPath)
     const privateKey = _this.bchjs.HDNode.toWIF(childNode)
 
-    // if (this.advancedOptions.password) {
-    //   this.mnemonicEncrypted = SimpleWallet.encrypt(mnemonic, this.advancedOptions.password);
-    // }
+    // Encrypt the mnemonic if a password is provided.
+    if (this.advancedOptions.password) {
+      this.mnemonicEncrypted = this.encrypt(mnemonic, this.advancedOptions.password)
+    }
 
+    // Set the wallet properties.
     this.mnemonic = mnemonic
     this.privateKey = privateKey
-    this.address = this.cashAddress = _this.bchjs.HDNode.toCashAddress(childNode)
+    this.address = this.cashAddress = _this.bchjs.HDNode.toCashAddress(
+      childNode
+    )
     this.slpAddress = _this.bchjs.SLP.Address.toSLPAddress(this.address)
     this.legacyAddress = _this.bchjs.HDNode.toLegacyAddress(childNode)
+  }
+
+  decrypt (mnemonicEncrypted, password) {
+    let mnemonic
+
+    try {
+      mnemonic = this.crypto.AES.decrypt(mnemonicEncrypted, password).toString(
+        this.crypto.enc.Utf8
+      )
+    } catch (err) {
+      throw new Error('Wrong password')
+    }
+
+    return mnemonic
+  }
+
+  encrypt (mnemonic, password) {
+    return this.crypto.AES.encrypt(mnemonic, password).toString()
   }
 }
 
--- test/unit/a01-minimal-bch-wallet-unit.js
+++ test/unit/a01-minimal-bch-wallet-unit.js
@@ -21,8 +21,8 @@ describe('#index.js - Minimal BCH Wallet', () => {
   afterEach(() => sandbox.restore())
 
   describe('#create', () => {
-    it('should create a wallet with no input', () => {
-      uut.create()
+    it('should create a new wallet with no input', async () => {
+      await uut.create()
       // console.log('uut: ', uut)
 
       assert.property(uut, 'mnemonic')
@@ -44,6 +44,131 @@ describe('#index.js - Minimal BCH Wallet', () => {
       assert.property(uut, 'slpAddress')
       assert.isString(uut.slpAddress)
       assert.isNotEmpty(uut.slpAddress)
+    })
+  })
+
+  describe('#constructor', () => {
+    it('should create a new wallet without encrypted mnemonic', async () => {
+      const uut = new MinimalBCHWallet()
+      await uut.walletInfo
+      // console.log('uut: ', uut)
+
+      assert.property(uut, 'mnemonic')
+      assert.isString(uut.mnemonic)
+      assert.isNotEmpty(uut.mnemonic)
+
+      assert.property(uut, 'privateKey')
+      assert.isString(uut.privateKey)
+      assert.isNotEmpty(uut.privateKey)
+
+      assert.property(uut, 'cashAddress')
+      assert.isString(uut.cashAddress)
+      assert.isNotEmpty(uut.cashAddress)
+
+      assert.property(uut, 'legacyAddress')
+      assert.isString(uut.legacyAddress)
+      assert.isNotEmpty(uut.legacyAddress)
+
+      assert.property(uut, 'slpAddress')
+      assert.isString(uut.slpAddress)
+      assert.isNotEmpty(uut.slpAddress)
+
+      assert.notProperty(uut, 'mnemonicEncrypted')
+    })
+
+    it('should create a new wallet with encrypted mnemonic', async () => {
+      const uut = new MinimalBCHWallet(null, { password: 'myStrongPassword' })
+      await uut.walletInfo
+      // console.log('uut: ', uut)
+
+      assert.property(uut, 'mnemonic')
+      assert.isString(uut.mnemonic)
+      assert.isNotEmpty(uut.mnemonic)
+
+      assert.property(uut, 'privateKey')
+      assert.isString(uut.privateKey)
+      assert.isNotEmpty(uut.privateKey)
+
+      assert.property(uut, 'cashAddress')
+      assert.isString(uut.cashAddress)
+      assert.isNotEmpty(uut.cashAddress)
+
+      assert.property(uut, 'legacyAddress')
+      assert.isString(uut.legacyAddress)
+      assert.isNotEmpty(uut.legacyAddress)
+
+      assert.property(uut, 'slpAddress')
+      assert.isString(uut.slpAddress)
+      assert.isNotEmpty(uut.slpAddress)
+
+      assert.property(uut, 'mnemonicEncrypted')
+      assert.isString(uut.mnemonicEncrypted)
+      assert.isNotEmpty(uut.mnemonicEncrypted)
+    })
+
+    it('should decrypt an encrypted mnemonic', async () => {
+      const mnemonicEncrypted =
+        'U2FsdGVkX18uyavim4FoIETcRxgOi1E/XFc1ARR3k6HVrJgH60YnLxjbs6yMnWMjpaqbBmSC3uYjhZ+cgFlndOEZI34T0sWFfL952CHCFjd2AjypCjFhqkmHzOCCkhgf'
+      const mnemonic =
+        'negative prepare champion corn bean proof one same column water warm melt'
+      const password = 'myStrongPassword'
+
+      const uut = new MinimalBCHWallet(mnemonicEncrypted, {
+        password: password
+      })
+      await uut.walletInfo
+      // console.log('uut: ', uut)
+
+      assert.equal(uut.mnemonic, mnemonic)
+    })
+
+    it('should throw error if incorrect password', async () => {
+      try {
+        const mnemonicEncrypted =
+          'U2FsdGVkX18uyavim4FoIETcRxgOi1E/XFc1ARR3k6HVrJgH60YnLxjbs6yMnWMjpaqbBmSC3uYjhZ+cgFlndOEZI34T0sWFfL952CHCFjd2AjypCjFhqkmHzOCCkhgf'
+
+        const uut = new MinimalBCHWallet(mnemonicEncrypted, {
+          password: 'bad password'
+        })
+        await uut.walletInfo
+
+        assert.notProperty(uut, 'mnemonic', 'Unexpected result!')
+      } catch (err) {
+        // console.log('err: ', err)
+        assert.include(err.message, 'Wrong password')
+      }
+    })
+
+    it('should import clear-text wallet mnemonic', async () => {
+      const mnemonic =
+        'negative prepare champion corn bean proof one same column water warm melt'
+
+      const uut = new MinimalBCHWallet(mnemonic)
+      await uut.walletInfo
+      // console.log('uut: ', uut)
+
+      assert.property(uut, 'mnemonic')
+      assert.isString(uut.mnemonic)
+      assert.isNotEmpty(uut.mnemonic)
+      assert.equal(uut.mnemonic, mnemonic)
+
+      assert.property(uut, 'privateKey')
+      assert.isString(uut.privateKey)
+      assert.isNotEmpty(uut.privateKey)
+
+      assert.property(uut, 'cashAddress')
+      assert.isString(uut.cashAddress)
+      assert.isNotEmpty(uut.cashAddress)
+
+      assert.property(uut, 'legacyAddress')
+      assert.isString(uut.legacyAddress)
+      assert.isNotEmpty(uut.legacyAddress)
+
+      assert.property(uut, 'slpAddress')
+      assert.isString(uut.slpAddress)
+      assert.isNotEmpty(uut.slpAddress)
+
+      assert.notProperty(uut, 'mnemonicEncrypted')
     })
   })
 })
