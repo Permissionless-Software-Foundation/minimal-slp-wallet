d166c477b817ceeeb71de349e368b05205130f05
Chris Troutner <chris.troutner@gmail.com>
2021-03-17 20:11:58 -0700

    fix(tokens.createTransaction): Updating unit tests

--- test/unit/a03-utxos-unit.js
+++ test/unit/a03-utxos-unit.js
@@ -28,56 +28,56 @@ describe('#UTXOs', () => {
 
   afterEach(() => sandbox.restore())
 
-  describe('#getUtxos', () => {
-    it('should get UTXO information for an address', async () => {
-      // Mock network calls.
-      sandbox.stub(uut.bchjs.Electrumx, 'utxo').resolves(mockData.simpleUtxos)
-
-      const addr = 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c'
-
-      const utxos = await uut.getUtxos(addr)
-      // console.log('utxos: ', utxos)
-
-      assert.isArray(utxos)
-      assert.property(utxos[0], 'height')
-      assert.property(utxos[0], 'tx_hash')
-      assert.property(utxos[0], 'tx_pos')
-      assert.property(utxos[0], 'value')
-    })
-
-    it('should handle network errors', async () => {
-      try {
-        sandbox
-          .stub(uut.bchjs.Electrumx, 'utxo')
-          .throws(new Error('test error'))
-
-        const addr = 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c'
-
-        await uut.getUtxos(addr)
-
-        assert(true, false, 'unexpected result')
-      } catch (err) {
-        assert.include(err.message, 'test error')
-      }
-    })
-
-    it('should handle unsuccesful queries', async () => {
-      try {
-        sandbox
-          .stub(uut.bchjs.Electrumx, 'utxo')
-          .resolves({ success: false, message: 'error message' })
-
-        const addr = 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c'
-
-        await uut.getUtxos(addr)
-
-        assert(true, false, 'unexpected result')
-      } catch (err) {
-        // console.log('err: ', err)
-        assert.include(err.message, 'Could not get UTXOs')
-      }
-    })
-  })
+  // describe('#getUtxos', () => {
+  //   it('should get UTXO information for an address', async () => {
+  //     // Mock network calls.
+  //     sandbox.stub(uut.bchjs.Electrumx, 'utxo').resolves(mockData.simpleUtxos)
+  //
+  //     const addr = 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c'
+  //
+  //     const utxos = await uut.getUtxos(addr)
+  //     // console.log('utxos: ', utxos)
+  //
+  //     assert.isArray(utxos)
+  //     assert.property(utxos[0], 'height')
+  //     assert.property(utxos[0], 'tx_hash')
+  //     assert.property(utxos[0], 'tx_pos')
+  //     assert.property(utxos[0], 'value')
+  //   })
+  //
+  //   it('should handle network errors', async () => {
+  //     try {
+  //       sandbox
+  //         .stub(uut.bchjs.Electrumx, 'utxo')
+  //         .throws(new Error('test error'))
+  //
+  //       const addr = 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c'
+  //
+  //       await uut.getUtxos(addr)
+  //
+  //       assert(true, false, 'unexpected result')
+  //     } catch (err) {
+  //       assert.include(err.message, 'test error')
+  //     }
+  //   })
+  //
+  //   it('should handle unsuccesful queries', async () => {
+  //     try {
+  //       sandbox
+  //         .stub(uut.bchjs.Electrumx, 'utxo')
+  //         .resolves({ success: false, message: 'error message' })
+  //
+  //       const addr = 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c'
+  //
+  //       await uut.getUtxos(addr)
+  //
+  //       assert(true, false, 'unexpected result')
+  //     } catch (err) {
+  //       // console.log('err: ', err)
+  //       assert.include(err.message, 'Could not get UTXOs')
+  //     }
+  //   })
+  // })
 
   describe('#getArrayChunks', () => {
     it('should throw error if "arrayToSlice" input is not array type', async () => {
@@ -227,44 +227,44 @@ describe('#UTXOs', () => {
     })
   })
 
-  describe('#initUtxoStore', () => {
-    it('should initialize and return the utxoStore', async () => {
-      const addr = 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c'
-
-      // Mock network calls.
-      sandbox.stub(uut, 'getUtxos').resolves(mockData.mixedUtxos)
-      // sandbox.stub(uut, 'hydrate').resolves(mockData.hydratedUtxos)
-      sandbox
-        .stub(uut.bchjs.SLP.Utils, 'hydrateUtxos')
-        .resolves({ slpUtxos: [{ utxos: mockData.hydratedUtxos }] })
-
-      const utxos = await uut.initUtxoStore(addr)
-
-      // Each UTXO returned should have an isValid property.
-      utxos.forEach(elem => assert.property(elem, 'isValid'))
-
-      // The UTXO store should match the returned data.
-      assert.deepEqual(uut.utxoStore, utxos)
-
-      // Token and BCH UTXOs should be separate.
-      assert.equal(uut.bchUtxos.length, 1)
-      assert.equal(uut.tokenUtxos.length, 2)
-    })
-
-    it('should handle network errors', async () => {
-      try {
-        const addr = 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c'
-
-        sandbox.stub(uut, 'getUtxos').throws(new Error('test error'))
-
-        await uut.initUtxoStore(addr)
-
-        assert(true, false, 'unexpected result')
-      } catch (err) {
-        assert.include(err.message, 'test error')
-      }
-    })
-  })
+  // describe('#initUtxoStore', () => {
+  //   it('should initialize and return the utxoStore', async () => {
+  //     const addr = 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c'
+  //
+  //     // Mock network calls.
+  //     sandbox.stub(uut, 'getUtxos').resolves(mockData.mixedUtxos)
+  //     // sandbox.stub(uut, 'hydrate').resolves(mockData.hydratedUtxos)
+  //     sandbox
+  //       .stub(uut.bchjs.SLP.Utils, 'hydrateUtxos')
+  //       .resolves({ slpUtxos: [{ utxos: mockData.hydratedUtxos }] })
+  //
+  //     const utxos = await uut.initUtxoStore(addr)
+  //
+  //     // Each UTXO returned should have an isValid property.
+  //     utxos.forEach(elem => assert.property(elem, 'isValid'))
+  //
+  //     // The UTXO store should match the returned data.
+  //     assert.deepEqual(uut.utxoStore, utxos)
+  //
+  //     // Token and BCH UTXOs should be separate.
+  //     assert.equal(uut.bchUtxos.length, 1)
+  //     assert.equal(uut.tokenUtxos.length, 2)
+  //   })
+  //
+  //   it('should handle network errors', async () => {
+  //     try {
+  //       const addr = 'bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c'
+  //
+  //       sandbox.stub(uut, 'getUtxos').throws(new Error('test error'))
+  //
+  //       await uut.initUtxoStore(addr)
+  //
+  //       assert(true, false, 'unexpected result')
+  //     } catch (err) {
+  //       assert.include(err.message, 'test error')
+  //     }
+  //   })
+  // })
 
   // describe('#diffUtxos', () => {
   //   it('should return an empty array when utxos are the same', () => {
--- test/unit/mocks/utxo-mocks.js
+++ test/unit/mocks/utxo-mocks.js
@@ -135,7 +135,19 @@ const hydratedUtxos = [
 
 const tokenUtxos01 = {
   address: 'bitcoincash:qqmjqwsplscmx0aet355p4l0j8q74thv7v90j65htu',
-  bchUtxos: [],
+  bchUtxos: [
+    {
+      height: 639762,
+      tx_hash:
+        '7fd05098bad5acb6c3ec15723227eeaf0437615a5665e5715cbc4a54a67dfe6b',
+      tx_pos: 4,
+      value: 2960,
+      satoshis: 2960,
+      txid: '7fd05098bad5acb6c3ec15723227eeaf0437615a5665e5715cbc4a54a67dfe6b',
+      vout: 4,
+      isValid: false
+    }
+  ],
   nullUtxos: [],
   slpUtxos: {
     type1: {
@@ -143,14 +155,17 @@ const tokenUtxos01 = {
       tokens: [
         {
           height: 640005,
-          tx_hash: '3ad72d9709c206329aa40777d9394ab6d0b5bd0962fba7180533fc966ece165c',
+          tx_hash:
+            '3ad72d9709c206329aa40777d9394ab6d0b5bd0962fba7180533fc966ece165c',
           tx_pos: 1,
           value: 546,
-          txid: '3ad72d9709c206329aa40777d9394ab6d0b5bd0962fba7180533fc966ece165c',
+          txid:
+            '3ad72d9709c206329aa40777d9394ab6d0b5bd0962fba7180533fc966ece165c',
           vout: 1,
           utxoType: 'token',
           transactionType: 'send',
-          tokenId: 'a4fb5c2da1aa064e25018a43f9165040071d9e984ba190c222a7f59053af84b2',
+          tokenId:
+            'a4fb5c2da1aa064e25018a43f9165040071d9e984ba190c222a7f59053af84b2',
           tokenTicker: 'TROUT',
           tokenName: "Trout's test token",
           tokenDocumentUrl: 'troutsblog.com',
@@ -162,42 +177,48 @@ const tokenUtxos01 = {
         },
         {
           height: 639443,
-          tx_hash: '8962566e413501224d178a02effc89be5ac0d8e4195f617415d443dc4c38fe50',
+          tx_hash:
+            '8962566e413501224d178a02effc89be5ac0d8e4195f617415d443dc4c38fe50',
           tx_pos: 1,
           value: 546,
           satoshis: 546,
-          txid: '8962566e413501224d178a02effc89be5ac0d8e4195f617415d443dc4c38fe50',
+          txid:
+            '8962566e413501224d178a02effc89be5ac0d8e4195f617415d443dc4c38fe50',
           vout: 1,
           utxoType: 'token',
           transactionType: 'send',
-          tokenId: '497291b8a1dfe69c8daea50677a3d31a5ef0e9484d8bebb610dac64bbc202fb7',
+          tokenId:
+            '497291b8a1dfe69c8daea50677a3d31a5ef0e9484d8bebb610dac64bbc202fb7',
           tokenTicker: 'TOK-CH',
           tokenName: 'TokyoCash',
           tokenDocumentUrl: '',
           tokenDocumentHash: '',
           decimals: 8,
           tokenType: 1,
-          tokenQty: 1,
+          tokenQty: '1',
           isValid: true
         },
         {
           height: 639443,
-          tx_hash: 'cbbc6106fdf6e21d84d66e06a1f7fcf3568817ddaa020ad1939db6ce0cfbe54b',
+          tx_hash:
+            'cbbc6106fdf6e21d84d66e06a1f7fcf3568817ddaa020ad1939db6ce0cfbe54b',
           tx_pos: 1,
           value: 546,
           satoshis: 546,
-          txid: 'cbbc6106fdf6e21d84d66e06a1f7fcf3568817ddaa020ad1939db6ce0cfbe54b',
+          txid:
+            'cbbc6106fdf6e21d84d66e06a1f7fcf3568817ddaa020ad1939db6ce0cfbe54b',
           vout: 1,
           utxoType: 'token',
           transactionType: 'send',
-          tokenId: 'd0feefd514ae9262a2030e50c6b6d4533000abd12d84bc48a50ba6d69c033c95',
+          tokenId:
+            'd0feefd514ae9262a2030e50c6b6d4533000abd12d84bc48a50ba6d69c033c95',
           tokenTicker: 'PRO',
           tokenName: 'PROPHET',
           tokenDocumentUrl: '',
           tokenDocumentHash: '',
           decimals: 0,
           tokenType: 1,
-          tokenQty: 1,
+          tokenQty: '1',
           isValid: true
         }
       ]
@@ -343,71 +364,134 @@ const mockTokenUtxos = [
   }
 ]
 
-const mockNFTGroupUtxos = [
-  {
-    height: 0,
-    tx_hash: 'ed934cf70830fda6c2a0b00e8e9d797172ff459c6dcd9112710fa6bd87f02aae',
-    tx_pos: 1,
-    value: 546,
-    satoshis: 546,
-    txid: 'ed934cf70830fda6c2a0b00e8e9d797172ff459c6dcd9112710fa6bd87f02aae',
-    vout: 1,
-    utxoType: 'token',
-    transactionType: 'send',
-    tokenId: '8cd26481aaed66198e22e05450839fda763daadbb9938b0c71521ef43c642299',
-    tokenTicker: 'NFTTT',
-    tokenName: 'NFT Test Token',
-    tokenDocumentUrl: 'https://FullStack.cash',
-    tokenDocumentHash: '',
-    decimals: 0,
-    tokenType: 129,
-    tokenQty: 1,
-    isValid: true
-  },
-  {
-    height: 0,
-    tx_hash: 'ed934cf70830fda6c2a0b00e8e9d797172ff459c6dcd9112710fa6bd87f02aae',
-    tx_pos: 3,
-    value: 23990,
-    satoshis: 23990,
-    txid: 'ed934cf70830fda6c2a0b00e8e9d797172ff459c6dcd9112710fa6bd87f02aae',
-    vout: 3,
-    isValid: false
+const mockNFTGroupUtxos = {
+  address: 'bitcoincash:qqmjqwsplscmx0aet355p4l0j8q74thv7v90j65htu',
+  bchUtxos: [
+    {
+      height: 639762,
+      tx_hash:
+        '7fd05098bad5acb6c3ec15723227eeaf0437615a5665e5715cbc4a54a67dfe6b',
+      tx_pos: 4,
+      value: 2960,
+      satoshis: 2960,
+      txid: '7fd05098bad5acb6c3ec15723227eeaf0437615a5665e5715cbc4a54a67dfe6b',
+      vout: 4,
+      isValid: false
+    }
+  ],
+  nullUtxos: [],
+  slpUtxos: {
+    type1: {
+      mintBatons: [],
+      tokens: [
+        {
+          height: 640005,
+          tx_hash:
+            '3ad72d9709c206329aa40777d9394ab6d0b5bd0962fba7180533fc966ece165c',
+          tx_pos: 1,
+          value: 546,
+          txid:
+            '3ad72d9709c206329aa40777d9394ab6d0b5bd0962fba7180533fc966ece165c',
+          vout: 1,
+          utxoType: 'token',
+          transactionType: 'send',
+          tokenId:
+            'a4fb5c2da1aa064e25018a43f9165040071d9e984ba190c222a7f59053af84b2',
+          tokenTicker: 'TROUT',
+          tokenName: "Trout's test token",
+          tokenDocumentUrl: 'troutsblog.com',
+          tokenDocumentHash: '',
+          decimals: 2,
+          tokenType: 1,
+          isValid: true,
+          tokenQty: '1'
+        }
+      ]
+    },
+    nft: {
+      groupMintBatons: [],
+      groupTokens: [
+        {
+          height: 0,
+          tx_hash:
+            'ed934cf70830fda6c2a0b00e8e9d797172ff459c6dcd9112710fa6bd87f02aae',
+          tx_pos: 1,
+          value: 546,
+          satoshis: 546,
+          txid:
+            'ed934cf70830fda6c2a0b00e8e9d797172ff459c6dcd9112710fa6bd87f02aae',
+          vout: 1,
+          utxoType: 'token',
+          transactionType: 'send',
+          tokenId:
+            '8cd26481aaed66198e22e05450839fda763daadbb9938b0c71521ef43c642299',
+          tokenTicker: 'NFTTT',
+          tokenName: 'NFT Test Token',
+          tokenDocumentUrl: 'https://FullStack.cash',
+          tokenDocumentHash: '',
+          decimals: 0,
+          tokenType: 129,
+          tokenQty: 1,
+          isValid: true
+        }
+      ],
+      tokens: []
+    }
   }
-]
+}
 
-const mockNFTChildUtxos = [
-  {
-    height: 650696,
-    tx_hash: '6458885509a8eec6b4e10a515d3834638acc7e6e49b9b5969ee2a1d03224565d',
-    tx_pos: 1,
-    value: 546,
-    satoshis: 546,
-    txid: '6458885509a8eec6b4e10a515d3834638acc7e6e49b9b5969ee2a1d03224565d',
-    vout: 1,
-    utxoType: 'token',
-    transactionType: 'send',
-    tokenId: '82e3d97b3cd033e60ffa755450b9075cf44fe1b2f6d5dc13657d8263e716b6a5',
-    tokenTicker: 'NFT004',
-    tokenName: 'NFT Child',
-    tokenDocumentUrl: 'https://FullStack.cash',
-    tokenDocumentHash: '',
-    decimals: 0,
-    tokenType: 65,
-    tokenQty: 1,
-    isValid: true
-  },
-  {
-    height: 650696,
-    tx_hash: 'c7678c16fd9ed9ef7f3a36b69aa7628e39afbbecc7e8d7667e44f4a9e7a8a7c2',
-    tx_pos: 3,
-    value: 21230,
-    satoshis: 21230,
-    txid: 'c7678c16fd9ed9ef7f3a36b69aa7628e39afbbecc7e8d7667e44f4a9e7a8a7c2',
-    vout: 3,
-    isValid: false
+const mockNFTChildUtxos = {
+  address: 'bitcoincash:qqmjqwsplscmx0aet355p4l0j8q74thv7v90j65htu',
+  bchUtxos: [
+    {
+      height: 639762,
+      tx_hash:
+        '7fd05098bad5acb6c3ec15723227eeaf0437615a5665e5715cbc4a54a67dfe6b',
+      tx_pos: 4,
+      value: 2960,
+      satoshis: 2960,
+      txid: '7fd05098bad5acb6c3ec15723227eeaf0437615a5665e5715cbc4a54a67dfe6b',
+      vout: 4,
+      isValid: false
+    }
+  ],
+  nullUtxos: [],
+  slpUtxos: {
+    type1: {
+      mintBatons: [],
+      tokens: []
+    },
+    nft: {
+      groupMintBatons: [],
+      groupTokens: [],
+      tokens: [
+        {
+          height: 650696,
+          tx_hash:
+            '6458885509a8eec6b4e10a515d3834638acc7e6e49b9b5969ee2a1d03224565d',
+          tx_pos: 1,
+          value: 546,
+          satoshis: 546,
+          txid:
+            '6458885509a8eec6b4e10a515d3834638acc7e6e49b9b5969ee2a1d03224565d',
+          vout: 1,
+          utxoType: 'token',
+          transactionType: 'send',
+          tokenId:
+            '82e3d97b3cd033e60ffa755450b9075cf44fe1b2f6d5dc13657d8263e716b6a5',
+          tokenTicker: 'NFT004',
+          tokenName: 'NFT Child',
+          tokenDocumentUrl: 'https://FullStack.cash',
+          tokenDocumentHash: '',
+          decimals: 0,
+          tokenType: 65,
+          tokenQty: 1,
+          isValid: true
+        }
+      ]
+    }
   }
-]
+}
 
 const dustAttackUtxo = {
   height: 655965,
@@ -475,68 +559,104 @@ const cornerCase1BchUtxos = [
   }
 ]
 
-const cornerCase1TokenUtxos = [
-  {
-    height: 657466,
-    tx_hash: 'ad5c4626297068f28d9ba6d45fb218cb622911e07ebe3003be33e9b7e8f0bc7f',
-    tx_pos: 2,
-    value: 546,
-    satoshis: 546,
-    txid: 'ad5c4626297068f28d9ba6d45fb218cb622911e07ebe3003be33e9b7e8f0bc7f',
-    vout: 2,
-    utxoType: 'token',
-    transactionType: 'send',
-    tokenId: '1a1fd545b922c8ee4ecd89bc312904f4e3ba4cf7850141066ad3e3f281668188',
-    tokenTicker: 'MINT',
-    tokenName: 'Mint',
-    tokenDocumentUrl: 'mintslp.com',
-    tokenDocumentHash: '',
-    decimals: 8,
-    tokenType: 1,
-    tokenQty: 46,
-    isValid: true
-  },
-  {
-    height: 0,
-    tx_hash: '99c9061f9afb8a201d32edfca2a5f3e753f8d218c69aa085d23ecf86c2a5744b',
-    tx_pos: 1,
-    value: 546,
-    satoshis: 546,
-    txid: '99c9061f9afb8a201d32edfca2a5f3e753f8d218c69aa085d23ecf86c2a5744b',
-    vout: 1,
-    utxoType: 'token',
-    transactionType: 'send',
-    tokenId: '38e97c5d7d3585a2cbf3f9580c82ca33985f9cb0845d4dcce220cb709f9538b0',
-    tokenTicker: 'PSF',
-    tokenName: 'Permissionless Software Foundation',
-    tokenDocumentUrl: 'psfoundation.cash',
-    tokenDocumentHash: '',
-    decimals: 8,
-    tokenType: 1,
-    tokenQty: 196,
-    isValid: true
-  },
-  {
-    height: 0,
-    tx_hash: '99c9061f9afb8a201d32edfca2a5f3e753f8d218c69aa085d23ecf86c2a5744b',
-    tx_pos: 2,
-    value: 546,
-    satoshis: 546,
-    txid: '99c9061f9afb8a201d32edfca2a5f3e753f8d218c69aa085d23ecf86c2a5744b',
-    vout: 2,
-    utxoType: 'token',
-    transactionType: 'send',
-    tokenId: '38e97c5d7d3585a2cbf3f9580c82ca33985f9cb0845d4dcce220cb709f9538b0',
-    tokenTicker: 'PSF',
-    tokenName: 'Permissionless Software Foundation',
-    tokenDocumentUrl: 'psfoundation.cash',
-    tokenDocumentHash: '',
-    decimals: 8,
-    tokenType: 1,
-    tokenQty: 0.14152798,
-    isValid: true
+const cornerCase1TokenUtxos = {
+  address: 'bitcoincash:qqmjqwsplscmx0aet355p4l0j8q74thv7v90j65htu',
+  bchUtxos: [
+    {
+      height: 639762,
+      tx_hash:
+        '7fd05098bad5acb6c3ec15723227eeaf0437615a5665e5715cbc4a54a67dfe6b',
+      tx_pos: 4,
+      value: 2960,
+      satoshis: 2960,
+      txid: '7fd05098bad5acb6c3ec15723227eeaf0437615a5665e5715cbc4a54a67dfe6b',
+      vout: 4,
+      isValid: false
+    }
+  ],
+  nullUtxos: [],
+  slpUtxos: {
+    type1: {
+      mintBatons: [],
+      tokens: [
+        {
+          height: 657466,
+          tx_hash:
+            'ad5c4626297068f28d9ba6d45fb218cb622911e07ebe3003be33e9b7e8f0bc7f',
+          tx_pos: 2,
+          value: 546,
+          satoshis: 546,
+          txid:
+            'ad5c4626297068f28d9ba6d45fb218cb622911e07ebe3003be33e9b7e8f0bc7f',
+          vout: 2,
+          utxoType: 'token',
+          transactionType: 'send',
+          tokenId:
+            '1a1fd545b922c8ee4ecd89bc312904f4e3ba4cf7850141066ad3e3f281668188',
+          tokenTicker: 'MINT',
+          tokenName: 'Mint',
+          tokenDocumentUrl: 'mintslp.com',
+          tokenDocumentHash: '',
+          decimals: 8,
+          tokenType: 1,
+          tokenQty: 46,
+          isValid: true
+        },
+        {
+          height: 0,
+          tx_hash:
+            '99c9061f9afb8a201d32edfca2a5f3e753f8d218c69aa085d23ecf86c2a5744b',
+          tx_pos: 1,
+          value: 546,
+          satoshis: 546,
+          txid:
+            '99c9061f9afb8a201d32edfca2a5f3e753f8d218c69aa085d23ecf86c2a5744b',
+          vout: 1,
+          utxoType: 'token',
+          transactionType: 'send',
+          tokenId:
+            '38e97c5d7d3585a2cbf3f9580c82ca33985f9cb0845d4dcce220cb709f9538b0',
+          tokenTicker: 'PSF',
+          tokenName: 'Permissionless Software Foundation',
+          tokenDocumentUrl: 'psfoundation.cash',
+          tokenDocumentHash: '',
+          decimals: 8,
+          tokenType: 1,
+          tokenQty: 196,
+          isValid: true
+        },
+        {
+          height: 0,
+          tx_hash:
+            '99c9061f9afb8a201d32edfca2a5f3e753f8d218c69aa085d23ecf86c2a5744b',
+          tx_pos: 2,
+          value: 546,
+          satoshis: 546,
+          txid:
+            '99c9061f9afb8a201d32edfca2a5f3e753f8d218c69aa085d23ecf86c2a5744b',
+          vout: 2,
+          utxoType: 'token',
+          transactionType: 'send',
+          tokenId:
+            '38e97c5d7d3585a2cbf3f9580c82ca33985f9cb0845d4dcce220cb709f9538b0',
+          tokenTicker: 'PSF',
+          tokenName: 'Permissionless Software Foundation',
+          tokenDocumentUrl: 'psfoundation.cash',
+          tokenDocumentHash: '',
+          decimals: 8,
+          tokenType: 1,
+          tokenQty: 0.14152798,
+          isValid: true
+        }
+      ]
+    },
+    nft: {
+      groupMintBatons: [],
+      groupTokens: [],
+      tokens: []
+    }
   }
-]
+}
 
 module.exports = {
   simpleUtxos,
