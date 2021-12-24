370919db9d2eccc942ab1ec089e9935dd9c42522
Chris Troutner <chris.troutner@gmail.com>
2021-12-24 12:35:49 -0800

    linting

--- test/unit/a05-adapters-router-unit.js
+++ test/unit/a05-adapters-router-unit.js
@@ -3,174 +3,174 @@
 */
 
 // Public npm libraries
-const assert = require("chai").assert;
-const sinon = require("sinon");
-const BCHJS = require("@psf/bch-js");
+const assert = require('chai').assert
+const sinon = require('sinon')
+const BCHJS = require('@psf/bch-js')
 
 // const Tokens = require('../../lib/tokens')
 // const Utxos = require('../../lib/utxos')
 
-const AdapterRouter = require("../../lib/adapters/router");
+const AdapterRouter = require('../../lib/adapters/router')
 
-let uut;
+let uut
 
 // const mockDataLib = require('./mocks/utxo-mocks')
 // let mockData
 // const sendMockDataLib = require('./mocks/send-bch-mocks')
 // let sendMockData
 
-describe("#adapter-router", () => {
-  let sandbox;
+describe('#adapter-router', () => {
+  let sandbox
   // let utxos
 
   beforeEach(() => {
-    const bchjs = new BCHJS();
-    uut = new AdapterRouter({ bchjs });
+    const bchjs = new BCHJS()
+    uut = new AdapterRouter({ bchjs })
 
-    sandbox = sinon.createSandbox();
+    sandbox = sinon.createSandbox()
 
     // mockData = Object.assign({}, mockDataLib)
     // sendMockData = Object.assign({}, sendMockDataLib)
-  });
+  })
 
-  afterEach(() => sandbox.restore());
+  afterEach(() => sandbox.restore())
 
-  describe("#constructor", () => {
-    it("should throw an error if bch-js is not included", () => {
+  describe('#constructor', () => {
+    it('should throw an error if bch-js is not included', () => {
       try {
-        uut = new AdapterRouter();
+        uut = new AdapterRouter()
 
-        assert.fail("Unexpected code path");
+        assert.fail('Unexpected code path')
       } catch (err) {
         assert.include(
           err.message,
-          "Must pass instance of bch-js when instantiating AdapterRouter."
-        );
+          'Must pass instance of bch-js when instantiating AdapterRouter.'
+        )
       }
-    });
+    })
 
-    it("should select rest-api by default", () => {
-      const bchjs = new BCHJS();
-      uut = new AdapterRouter({ bchjs });
+    it('should select rest-api by default', () => {
+      const bchjs = new BCHJS()
+      uut = new AdapterRouter({ bchjs })
 
-      assert.equal(uut.interface, "rest-api");
-    });
+      assert.equal(uut.interface, 'rest-api')
+    })
 
-    it("should select consumer-api interface", () => {
-      const bchjs = new BCHJS();
-      uut = new AdapterRouter({ bchjs, interface: "consumer-api" });
+    it('should select consumer-api interface', () => {
+      const bchjs = new BCHJS()
+      uut = new AdapterRouter({ bchjs, interface: 'consumer-api' })
 
-      assert.equal(uut.interface, "consumer-api");
-    });
+      assert.equal(uut.interface, 'consumer-api')
+    })
 
-    it("should overwrite default walletService library", () => {
-      const bchjs = new BCHJS();
+    it('should overwrite default walletService library', () => {
+      const bchjs = new BCHJS()
 
-      const walletService = {};
+      const walletService = {}
 
       uut = new AdapterRouter({
         bchjs,
-        interface: "consumer-api",
+        interface: 'consumer-api',
         walletService
-      });
+      })
 
-      assert.equal(uut.interface, "consumer-api");
-    });
-  });
+      assert.equal(uut.interface, 'consumer-api')
+    })
+  })
 
-  describe("#getUtxos", () => {
-    it("should throw an error if address is not specified", async () => {
+  describe('#getUtxos', () => {
+    it('should throw an error if address is not specified', async () => {
       try {
-        await uut.getUtxos();
+        await uut.getUtxos()
 
-        assert.fail("Unexpected code path");
+        assert.fail('Unexpected code path')
       } catch (err) {
         assert.include(
           err.message,
-          "Address string required when calling getUtxos()"
-        );
+          'Address string required when calling getUtxos()'
+        )
       }
-    });
+    })
 
-    it("should use bch-js by default", async () => {
+    it('should use bch-js by default', async () => {
       // Mock dependencies.
-      sandbox.stub(uut.bchjs.Utxo, "get").resolves(["test str"]);
+      sandbox.stub(uut.bchjs.Utxo, 'get').resolves(['test str'])
 
-      const result = await uut.getUtxos("fake-addr");
+      const result = await uut.getUtxos('fake-addr')
 
-      assert.equal(result, "test str");
-    });
+      assert.equal(result, 'test str')
+    })
 
-    it("should use wallet service when consumer interface is selected", async () => {
+    it('should use wallet service when consumer interface is selected', async () => {
       // Mock dependencies.
-      sandbox.stub(uut.walletService, "getUtxos").resolves(["test str"]);
+      sandbox.stub(uut.walletService, 'getUtxos').resolves(['test str'])
 
-      uut.interface = "consumer-api";
+      uut.interface = 'consumer-api'
 
-      const result = await uut.getUtxos("fake-addr");
+      const result = await uut.getUtxos('fake-addr')
 
-      assert.equal(result, "test str");
-    });
+      assert.equal(result, 'test str')
+    })
 
-    it("should throw an error if an interface is not specified", async () => {
+    it('should throw an error if an interface is not specified', async () => {
       try {
-        uut.interface = "";
+        uut.interface = ''
 
-        await uut.getUtxos("fake-addr");
+        await uut.getUtxos('fake-addr')
 
-        assert.fail("Unexpected code path");
+        assert.fail('Unexpected code path')
       } catch (err) {
-        assert.include(err.message, "this.interface is not specified");
+        assert.include(err.message, 'this.interface is not specified')
       }
-    });
-  });
+    })
+  })
 
-  describe("#sendTx", () => {
-    it("should throw an error hex is not specified", async () => {
+  describe('#sendTx', () => {
+    it('should throw an error hex is not specified', async () => {
       try {
-        await uut.sendTx();
+        await uut.sendTx()
 
-        assert.fail("Unexpected code path");
+        assert.fail('Unexpected code path')
       } catch (err) {
         assert.include(
           err.message,
-          "Hex encoded transaction required as input."
-        );
+          'Hex encoded transaction required as input.'
+        )
       }
-    });
+    })
 
-    it("should use bch-js by default", async () => {
+    it('should use bch-js by default', async () => {
       // Mock dependencies.
       sandbox
-        .stub(uut.bchjs.RawTransactions, "sendRawTransaction")
-        .resolves("txid-str");
+        .stub(uut.bchjs.RawTransactions, 'sendRawTransaction')
+        .resolves('txid-str')
 
-      const result = await uut.sendTx("fakeHex");
+      const result = await uut.sendTx('fakeHex')
 
-      assert.equal(result, "txid-str");
-    });
+      assert.equal(result, 'txid-str')
+    })
 
-    it("should use wallet service when consumer-api interface is selected", async () => {
+    it('should use wallet service when consumer-api interface is selected', async () => {
       // Mock dependencies.
-      sandbox.stub(uut.walletService, "sendTx").resolves("txid-str");
+      sandbox.stub(uut.walletService, 'sendTx').resolves('txid-str')
 
-      uut.interface = "consumer-api";
+      uut.interface = 'consumer-api'
 
-      const result = await uut.sendTx("fakeHex");
+      const result = await uut.sendTx('fakeHex')
 
-      assert.equal(result, "txid-str");
-    });
+      assert.equal(result, 'txid-str')
+    })
 
-    it("should throw an error if an interface is not specified", async () => {
+    it('should throw an error if an interface is not specified', async () => {
       try {
-        uut.interface = "";
+        uut.interface = ''
 
-        await uut.sendTx("fake-addr");
+        await uut.sendTx('fake-addr')
 
-        assert.fail("Unexpected code path");
+        assert.fail('Unexpected code path')
       } catch (err) {
-        assert.include(err.message, "this.interface is not specified");
+        assert.include(err.message, 'this.interface is not specified')
       }
-    });
-  });
-});
+    })
+  })
+})
