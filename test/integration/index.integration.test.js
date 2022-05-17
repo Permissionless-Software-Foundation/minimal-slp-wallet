beefdad2aa36ce7bb0cfa5e105947471423cdbed
Chris Troutner <chris.troutner@gmail.com>
2022-05-16 17:26:56 -0700

    fix(tests): Added additional integration tests

--- test/integration/index.integration.test.js
+++ test/integration/index.integration.test.js
@@ -5,7 +5,10 @@
 // Public npm libraries.
 const assert = require('chai').assert
 
+// Local libraries
 const BchWallet = require('../../index')
+
+const restURL = 'https://free-bch.fullstack.cash'
 
 describe('#BchWallet', () => {
   let uut
@@ -70,6 +73,80 @@ describe('#BchWallet', () => {
       // console.log(`result: ${JSON.stringify(result, null, 2)}`);
 
       assert.isAbove(result, 546)
+    })
+  })
+
+  describe('#utxoIsValid', () => {
+    it('should return true for valid UTXO using fullstack interface', async () => {
+      const utxo = {
+        txid: 'b94e1ff82eb5781f98296f0af2488ff06202f12ee92b0175963b8dba688d1b40',
+        vout: 0
+      }
+
+      const result = await uut.utxoIsValid(utxo)
+
+      assert.equal(result, true)
+    })
+
+    it('should return true for valid UTXO using consumer-api interface', async () => {
+      uut = new BchWallet(undefined, { interface: 'consumer-api', restURL, noUpdate: true })
+
+      const utxo = {
+        txid: 'b94e1ff82eb5781f98296f0af2488ff06202f12ee92b0175963b8dba688d1b40',
+        vout: 0
+      }
+
+      const result = await uut.utxoIsValid(utxo)
+
+      assert.equal(result, true)
+    })
+
+    it('should return false for invalid UTXO using fullstack interface', async () => {
+      const utxo = {
+        txid: '17754221b29f189532d4fc2ae89fb467ad2dede30fdec4854eb2129b3ba90d7a',
+        vout: 0
+      }
+
+      const result = await uut.utxoIsValid(utxo)
+
+      assert.equal(result, false)
+    })
+
+    it('should return true for valid UTXO using consumer-api interface', async () => {
+      uut = new BchWallet(undefined, { interface: 'consumer-api', restURL, noUpdate: true })
+
+      const utxo = {
+        txid: '17754221b29f189532d4fc2ae89fb467ad2dede30fdec4854eb2129b3ba90d7a',
+        vout: 0
+      }
+
+      const result = await uut.utxoIsValid(utxo)
+
+      assert.equal(result, false)
+    })
+  })
+
+  describe('#getTokenData', () => {
+    it('should get token data from fullstack.cash', async () => {
+      const tokenId = 'eb93f05553ff088bffb0ec687519e83c59e5108c160f7c25a4b6c45109d7e40b'
+
+      const result = await uut.getTokenData(tokenId)
+      // console.log('result: ', result)
+
+      assert.include(result.immutableData, 'ipfs')
+      assert.include(result.mutableData, 'ipfs')
+    })
+
+    it('should get token data from free-bch', async () => {
+      uut = new BchWallet(undefined, { interface: 'consumer-api', restURL, noUpdate: true })
+      
+      const tokenId = 'eb93f05553ff088bffb0ec687519e83c59e5108c160f7c25a4b6c45109d7e40b'
+
+      const result = await uut.getTokenData(tokenId)
+      // console.log('result: ', result)
+
+      assert.include(result.immutableData, 'ipfs')
+      assert.include(result.mutableData, 'ipfs')
     })
   })
 })
--- test/integration/router.integration.js
+++ test/integration/router.integration.js
@@ -10,7 +10,8 @@ const BCHJS = require('@psf/bch-js')
 // Local libraries
 const Router = require('../../lib/adapters/router')
 
-const restURL = 'http://localhost:5005'
+// const restURL = 'http://localhost:5005'
+const restURL = 'https://free-bch.fullstack.cash'
 
 describe('#router.js', () => {
   let uut
@@ -20,137 +21,137 @@ describe('#router.js', () => {
     uut = new Router({ bchjs })
   })
 
-  // describe('#getBalances', () => {
-  //   it('should get a balance from bch-js', async () => {
-  //     const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
-  //
-  //     const result = await uut.getBalance(addr)
-  //     // console.log(`result: ${JSON.stringify(result, null, 2)}`)
-  //
-  //     assert.equal(result.success, true)
-  //     assert.property(result.balance, 'confirmed')
-  //     assert.property(result.balance, 'unconfirmed')
-  //   })
-  //
-  //   it('should get a balance from bch-consumer', async () => {
-  //     const bchjs = new BCHJS()
-  //     uut = new Router({ bchjs, interface: 'consumer-api' })
-  //
-  //     const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
-  //
-  //     const result = await uut.getBalance(addr)
-  //     // console.log(`result: ${JSON.stringify(result, null, 2)}`)
-  //
-  //     assert.equal(result.success, true)
-  //     assert.property(result.balance, 'confirmed')
-  //     assert.property(result.balance, 'unconfirmed')
-  //   })
-  // })
-  //
-  // describe('#getUtxos', () => {
-  //   it('should get UTXOs from bch-js', async () => {
-  //     const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
-  //
-  //     const result = await uut.getUtxos(addr)
-  //     // console.log(`result: ${JSON.stringify(result, null, 2)}`)
-  //
-  //     assert.equal(result.address, addr)
-  //     assert.property(result, 'bchUtxos')
-  //     assert.property(result, 'slpUtxos')
-  //   })
-  //
-  //   it('should get UTXOs from bch-consumer', async () => {
-  //     const bchjs = new BCHJS()
-  //     uut = new Router({ bchjs, interface: 'consumer-api' })
-  //
-  //     const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
-  //
-  //     const result = await uut.getUtxos(addr)
-  //     // console.log(`result: ${JSON.stringify(result, null, 2)}`)
-  //
-  //     assert.equal(result.address, addr)
-  //     assert.property(result, 'bchUtxos')
-  //     assert.property(result, 'slpUtxos')
-  //   })
-  // })
-  //
-  // describe('#getTransactions', () => {
-  //   it('should get a transaction history from bch-js', async () => {
-  //     const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
-  //
-  //     const result = await uut.getTransactions(addr)
-  //     // console.log(`result: ${JSON.stringify(result, null, 2)}`)
-  //
-  //     assert.equal(result.success, true)
-  //     assert.property(result.transactions[0], 'height')
-  //     assert.property(result.transactions[0], 'tx_hash')
-  //   })
-  //
-  //   it('should get a transaction history from bch-consumer', async () => {
-  //     const bchjs = new BCHJS()
-  //     uut = new Router({ bchjs, interface: 'consumer-api' })
-  //
-  //     const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
-  //
-  //     const result = await uut.getTransactions(addr)
-  //     // console.log(`result: ${JSON.stringify(result, null, 2)}`)
-  //
-  //     assert.equal(result.success, true)
-  //     assert.property(result.transactions[0], 'height')
-  //     assert.property(result.transactions[0], 'tx_hash')
-  //   })
-  // })
-  //
-  // describe('#sendTx', () => {
-  //   it('should send a tx through bch-js', async () => {
-  //     try {
-  //       const hex =
-  //         '01000000013ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a000000006a4730440220540986d1c58d6e76f8f05501c520c38ce55393d0ed7ed3c3a82c69af04221232022058ea43ed6c05fec0eccce749a63332ed4525460105346f11108b9c26df93cd72012103083dfc5a0254613941ddc91af39ff90cd711cdcde03a87b144b883b524660c39ffffffff01807c814a000000001976a914d7e7c4e0b70eaa67ceff9d2823d1bbb9f6df9a5188ac00000000'
-  //
-  //       await uut.sendTx(hex)
-  //       // console.log(`result: ${JSON.stringify(result, null, 2)}`)
-  //
-  //       assert.fail('Unexpected code path')
-  //     } catch (err) {
-  //       assert.include(err.message, 'Missing inputs')
-  //     }
-  //   })
-  //
-  //   it('should send a tx through bch-consumer', async () => {
-  //     try {
-  //       const bchjs = new BCHJS()
-  //       uut = new Router({ bchjs, interface: 'consumer-api' })
-  //
-  //       const hex =
-  //         '01000000013ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a000000006a4730440220540986d1c58d6e76f8f05501c520c38ce55393d0ed7ed3c3a82c69af04221232022058ea43ed6c05fec0eccce749a63332ed4525460105346f11108b9c26df93cd72012103083dfc5a0254613941ddc91af39ff90cd711cdcde03a87b144b883b524660c39ffffffff01807c814a000000001976a914d7e7c4e0b70eaa67ceff9d2823d1bbb9f6df9a5188ac00000000'
-  //
-  //       await uut.sendTx(hex)
-  //
-  //       assert.fail('Unexpected code path')
-  //     } catch (err) {
-  //       assert.include(err.message, 'Missing inputs')
-  //     }
-  //   })
-  // })
-  //
-  // describe('#getUsd', () => {
-  //   it('should get USD price from bch-js', async () => {
-  //     const result = await uut.getUsd()
-  //     // console.log('result: ', result)
-  //
-  //     assert.isAbove(result, 0)
-  //   })
-  //
-  //   it('should get USD price from bch-consumer', async () => {
-  //     const bchjs = new BCHJS()
-  //     uut = new Router({ bchjs, interface: 'consumer-api' })
-  //
-  //     const result = await uut.getUsd()
-  //     // console.log('result: ', result)
-  //
-  //     assert.isAbove(result, 0)
-  //   })
-  // })
+  describe('#getBalances', () => {
+    it('should get a balance from bch-js', async () => {
+      const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
+
+      const result = await uut.getBalance(addr)
+      // console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+      assert.equal(result.success, true)
+      assert.property(result.balance, 'confirmed')
+      assert.property(result.balance, 'unconfirmed')
+    })
+
+    it('should get a balance from bch-consumer', async () => {
+      const bchjs = new BCHJS()
+      uut = new Router({ bchjs, interface: 'consumer-api' })
+
+      const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
+
+      const result = await uut.getBalance(addr)
+      // console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+      assert.equal(result.success, true)
+      assert.property(result.balance, 'confirmed')
+      assert.property(result.balance, 'unconfirmed')
+    })
+  })
+
+  describe('#getUtxos', () => {
+    it('should get UTXOs from bch-js', async () => {
+      const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
+
+      const result = await uut.getUtxos(addr)
+      // console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+      assert.equal(result.address, addr)
+      assert.property(result, 'bchUtxos')
+      assert.property(result, 'slpUtxos')
+    })
+
+    it('should get UTXOs from bch-consumer', async () => {
+      const bchjs = new BCHJS()
+      uut = new Router({ bchjs, interface: 'consumer-api' })
+
+      const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
+
+      const result = await uut.getUtxos(addr)
+      // console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+      assert.equal(result.address, addr)
+      assert.property(result, 'bchUtxos')
+      assert.property(result, 'slpUtxos')
+    })
+  })
+
+  describe('#getTransactions', () => {
+    it('should get a transaction history from bch-js', async () => {
+      const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
+
+      const result = await uut.getTransactions(addr)
+      // console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+      assert.equal(result.success, true)
+      assert.property(result.transactions[0], 'height')
+      assert.property(result.transactions[0], 'tx_hash')
+    })
+
+    it('should get a transaction history from bch-consumer', async () => {
+      const bchjs = new BCHJS()
+      uut = new Router({ bchjs, interface: 'consumer-api' })
+
+      const addr = 'bitcoincash:qrl2nlsaayk6ekxn80pq0ks32dya8xfclyktem2mqj'
+
+      const result = await uut.getTransactions(addr)
+      // console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+      assert.equal(result.success, true)
+      assert.property(result.transactions[0], 'height')
+      assert.property(result.transactions[0], 'tx_hash')
+    })
+  })
+
+  describe('#sendTx', () => {
+    it('should send a tx through bch-js', async () => {
+      try {
+        const hex =
+          '01000000013ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a000000006a4730440220540986d1c58d6e76f8f05501c520c38ce55393d0ed7ed3c3a82c69af04221232022058ea43ed6c05fec0eccce749a63332ed4525460105346f11108b9c26df93cd72012103083dfc5a0254613941ddc91af39ff90cd711cdcde03a87b144b883b524660c39ffffffff01807c814a000000001976a914d7e7c4e0b70eaa67ceff9d2823d1bbb9f6df9a5188ac00000000'
+
+        await uut.sendTx(hex)
+        // console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+        assert.fail('Unexpected code path')
+      } catch (err) {
+        assert.include(err.message, 'Missing inputs')
+      }
+    })
+
+    it('should send a tx through bch-consumer', async () => {
+      try {
+        const bchjs = new BCHJS()
+        uut = new Router({ bchjs, interface: 'consumer-api' })
+
+        const hex =
+          '01000000013ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a000000006a4730440220540986d1c58d6e76f8f05501c520c38ce55393d0ed7ed3c3a82c69af04221232022058ea43ed6c05fec0eccce749a63332ed4525460105346f11108b9c26df93cd72012103083dfc5a0254613941ddc91af39ff90cd711cdcde03a87b144b883b524660c39ffffffff01807c814a000000001976a914d7e7c4e0b70eaa67ceff9d2823d1bbb9f6df9a5188ac00000000'
+
+        await uut.sendTx(hex)
+
+        assert.fail('Unexpected code path')
+      } catch (err) {
+        assert.include(err.message, 'Missing inputs')
+      }
+    })
+  })
+
+  describe('#getUsd', () => {
+    it('should get USD price from bch-js', async () => {
+      const result = await uut.getUsd()
+      // console.log('result: ', result)
+
+      assert.isAbove(result, 0)
+    })
+
+    it('should get USD price from bch-consumer', async () => {
+      const bchjs = new BCHJS()
+      uut = new Router({ bchjs, interface: 'consumer-api' })
+
+      const result = await uut.getUsd()
+      // console.log('result: ', result)
+
+      assert.isAbove(result, 0)
+    })
+  })
 
   describe('#getTxData', () => {
     it('should get a TX data from bch-js', async () => {
