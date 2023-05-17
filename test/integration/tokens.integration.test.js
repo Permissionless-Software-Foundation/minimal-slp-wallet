f04c81da711d2387b01a30313c81b6c169579256
Chris Troutner <chris.troutner@gmail.com>
2023-05-17 09:42:25 -0700

    Working on getTokenBalance()

--- test/integration/tokens.integration.test.js
+++ test/integration/tokens.integration.test.js
@@ -25,7 +25,7 @@ describe('#tokens.js', () => {
       const addr = 'simpleledger:qqmjqwsplscmx0aet355p4l0j8q74thv7vf5epph4z'
 
       const tokenInfo = await uut.listTokensFromAddress(addr)
-      // console.log(`tokenInfo: ${JSON.stringify(tokenInfo, null, 2)}`)
+      console.log(`tokenInfo: ${JSON.stringify(tokenInfo, null, 2)}`)
 
       assert.isArray(tokenInfo)
 
@@ -42,9 +42,19 @@ describe('#tokens.js', () => {
       const addr = 'bitcoincash:qzjs5l0a3gvmfuqw9szs4glzpf4j63jjkvfj9hqedl'
 
       const tokenInfo = await uut.listTokensFromAddress(addr)
-      // console.log(`tokenInfo: ${JSON.stringify(tokenInfo, null, 2)}`)
+      console.log(`tokenInfo: ${JSON.stringify(tokenInfo, null, 2)}`)
 
       assert.equal(tokenInfo[0].tokenType, 65)
+    })
+  })
+
+  describe('#getTokenBalance', () => {
+    it('should get the token balance for a wallet', async () => {
+      const addr = 'simpleledger:qqmjqwsplscmx0aet355p4l0j8q74thv7vf5epph4z'
+      const tokenId = "a4fb5c2da1aa064e25018a43f9165040071d9e984ba190c222a7f59053af84b2"
+
+      const tokenBalance = await uut.getTokenBalance(tokenId, addr)
+      console.log(`tokenBalance: ${JSON.stringify(tokenBalance, null, 2)}`)
     })
   })
 })
